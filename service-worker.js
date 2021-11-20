1f1fa12dcfdb16541e0703bbb8c12109ab7f7c5c
Mihail Mikov <m.mikoff@gmail.com>
2021-11-20 09:44:29 +0200

    [FALL21] updated menu

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04255afb09349dbc8a13536448a2bebd.js"
+  "/precache-manifest.e9f81bdde6969e587e3279d7991ed5b3.js"
 );
 
 self.addEventListener('message', (event) => {
